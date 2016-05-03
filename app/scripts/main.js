7ce20b3782413c8585c51c71f6a57b9d07aba4c4
jayfid <jay@tommiecopper.com>
2016-05-03 16:35:06 -0400

    made mobile and desktop flipping more compatible added tacos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,55 @@
 /* credit - modified from http://jsfiddle.net/rsadwick/zwWHY/ */
-(function($) {
+(function($, window) {
     'use strict';
-
-    $('.flip').mouseover(function(){
-        $(this).find('.card').addClass('flipped');
+    var entered = false, tapped = false, keyindex = 0, phrase = [116, 97, 99, 111], tacoed = false;
+    $('.flip').mouseover(function() {
+        if ( !entered ) {
+            entered = true;
+            tapped = true;
+            $(this).find('.card').addClass('flipped');
+        }
     });
-    $('.flip').mouseleave(function(){
-        $(this).find('.card').removeClass('flipped');
+
+    $('.flip').mouseleave(function() {
+        if ( entered ) {
+            entered = false;
+            $(this).find('.card').removeClass('flipped');
+        }
     });
 
     window.setTimeout(function() {
         $('.card').addClass('flicked');
     }, 200);
+
     window.setTimeout(function () {
         $('.card').removeClass('flicked');
     }, 500);
 
     $('.frame').click(function() {
-        $('.card').toggleClass('flipped');
+        if (!tapped) {
+            $('.card').toggleClass('flipped');
+        }
     });
-})(jQuery);
+
+    window.setInterval(function () {
+        tapped = false;
+    }, 500);
+
+    $(window).keypress(function(e) {
+        if ( tacoed ) {
+            return;
+        }
+        var key = e.which;
+        if ( phrase[keyindex] === key ) {
+            keyindex++;
+            if ( keyindex === phrase.length ) {
+                $(document.body).addClass('taco');
+                tacoed = true;
+            }
+        }
+        else {
+            keyindex = 0;
+        }
+    });
+
+})(jQuery, window);
