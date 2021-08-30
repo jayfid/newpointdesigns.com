88d3c759b86a6da61107754eee3de8c2943759ec
jayfid <jayfid@protonmail.com>
2021-08-29 21:02:45 -0400

    add prettier

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,55 +1,57 @@
 /* credit - modified from http://jsfiddle.net/rsadwick/zwWHY/ */
-(function($, window) {
-    'use strict';
-    var entered = false, tapped = false, keyindex = 0, phrase = [116, 97, 99, 111], tacoed = false;
-    $('.flip').mouseover(function() {
-        if ( !entered ) {
-            entered = true;
-            tapped = true;
-            $(this).find('.card').addClass('flipped');
-        }
-    });
+(function ($, window) {
+  "use strict";
+  var entered = false,
+    tapped = false,
+    keyindex = 0,
+    phrase = [116, 97, 99, 111],
+    tacoed = false;
+  $(".flip").mouseover(function () {
+    if (!entered) {
+      entered = true;
+      tapped = true;
+      $(this).find(".card").addClass("flipped");
+    }
+  });
 
-    $('.flip').mouseleave(function() {
-        if ( entered ) {
-            entered = false;
-            $(this).find('.card').removeClass('flipped');
-        }
-    });
+  $(".flip").mouseleave(function () {
+    if (entered) {
+      entered = false;
+      $(this).find(".card").removeClass("flipped");
+    }
+  });
 
-    window.setTimeout(function() {
-        $('.card').addClass('flicked');
-    }, 200);
+  window.setTimeout(function () {
+    $(".card").addClass("flicked");
+  }, 200);
 
-    window.setTimeout(function () {
-        $('.card').removeClass('flicked');
-    }, 500);
+  window.setTimeout(function () {
+    $(".card").removeClass("flicked");
+  }, 500);
 
-    $('.frame').click(function() {
-        if (!tapped) {
-            $('.card').toggleClass('flipped');
-        }
-    });
+  $(".frame").click(function () {
+    if (!tapped) {
+      $(".card").toggleClass("flipped");
+    }
+  });
 
-    window.setInterval(function () {
-        tapped = false;
-    }, 500);
+  window.setInterval(function () {
+    tapped = false;
+  }, 500);
 
-    $(window).keypress(function(e) {
-        if ( tacoed ) {
-            return;
-        }
-        var key = e.which;
-        if ( phrase[keyindex] === key ) {
-            keyindex++;
-            if ( keyindex === phrase.length ) {
-                $(document.body).addClass('taco');
-                tacoed = true;
-            }
-        }
-        else {
-            keyindex = 0;
-        }
-    });
-
+  $(window).keypress(function (e) {
+    if (tacoed) {
+      return;
+    }
+    var key = e.which;
+    if (phrase[keyindex] === key) {
+      keyindex++;
+      if (keyindex === phrase.length) {
+        $(document.body).addClass("taco");
+        tacoed = true;
+      }
+    } else {
+      keyindex = 0;
+    }
+  });
 })(jQuery, window);
