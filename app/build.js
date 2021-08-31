08b1ee8544145d8dd58f15122e0edd9905484ec2
jayfid <jayfid@protonmail.com>
2021-08-30 23:24:16 -0400

    wip

--- app/build.js
+++
@@ -1,4 +0,0 @@
-// minify html
-// copy static files, images
-// minify scss
-// minify js
--- app/server.js
+++ app/server.js
@@ -1,12 +1,8 @@
 var express = require("express");
 var minify = require("express-minify");
 var minifyHTML = require("express-minify-html");
-express.static.mime.define({
-  "text/x-scss": ["scss"],
-});
-
+express.static.mime.define({ "text/x-scss": ["scss"] });
 var app = express();
-
 app.use(minifyHTML());
 app.set("view engine", "html");
 app.engine("html", require("ejs").renderFile);
@@ -15,27 +11,18 @@ app.get("/", function (req, res) {
     res.send(err || html);
   });
 });
-
 app.use(minify({ sass_match: /css/ }));
-app.get("/styles/main.css", function (req, res) {
-  req.url = "/styles/main.scss";
+app.get("/main.css", function (req, res) {
+  req.url = "/main.scss";
   app.handle(req, res);
 });
-app.use(function (req, res, next) {
-  if (/styles\/main\.scss$/.test(req.url)) {
-    res.minifyOptions = res.minifyOptions || {};
-    res.minifyOptions.sass = { includePaths: ["./styles"] };
-  }
-  next();
-});
-app.use("/styles", express.static(__dirname + "/styles"));
-app.use("/scripts", express.static(__dirname + "/scripts"));
+app.use("/main.scss", express.static(__dirname + "/main.scss"));
+app.use("/main.js", express.static(__dirname + "/main.js"));
 app.use("/images", express.static(__dirname + "/images"));
-app.use("/favicon.ico", express.static(__dirname + "/favicon.ico"));
+app.use("/favicon.ico", express.static(__dirname + "/assets/favicon.ico"));
 app.use(
   "/apple-touch-icon.png",
-  express.static(__dirname + "/apple-touch-icon.png")
+  express.static(__dirname + "/assets/apple-touch-icon.png")
 );
-
 app.listen(3000);
 console.debug("Server running at http://localhost:3000");
