e142eb01b75515e519d3b678d5691a8f2f917e7c
jayfid <jayfid@protonmail.com>
2022-05-16 23:08:50 -0400

    interactivity

--- src/main.js
+++ src/main.js
@@ -5,43 +5,58 @@ const foobar = () => {
   flickCard();
   taco();
 };
-const timeoutDuration = 300;
+
+class stateBox {
+  isLocked = false;
+
+  lock() {
+    this.isLocked = true;
+  }
+
+  unlock() {
+    this.isLocked = false;
+  }
+}
+const currentState = new stateBox();
+
+const handleEvent = (e) => {
+  currentState.lock();
+  e.preventDefault();
+  e.stopImmediatePropagation();
+  window.setTimeout(() => currentState.unlock(), 200);
+};
 
 const addListeners = () => {
-  let bufferTimeout = false;
-
-  const resetBufferTimeout = () => {
-    bufferTimeout = false;
-  };
   const flipElement = document.getElementsByClassName("flip")[0];
   const cardElement = document.getElementsByClassName("card")[0];
   flipElement.addEventListener(
     "mouseenter",
     (e) => {
-      if (bufferTimeout || e.target !== flipElement) return;
-      bufferTimeout = true;
+      if (e.target !== flipElement || currentState.isLocked) return;
+      handleEvent(e);
       cardElement.classList.add("flipped");
-      window.setTimeout(resetBufferTimeout, timeoutDuration);
     },
-    false
+    true
   );
   flipElement.addEventListener(
     "mouseleave",
     (e) => {
-      if (bufferTimeout && e.target !== flipElement) return;
-      bufferTimeout = true;
+      if (e.target !== flipElement || currentState.isLocked) return;
+      handleEvent(e);
       cardElement.classList.remove("flipped");
-      window.setTimeout(resetBufferTimeout, timeoutDuration);
     },
-    false
+    true
   );
   document.body.addEventListener(
     "mouseup",
-    () => {
-      if (bufferTimeout) return;
-      bufferTimeout = true;
+    (e) => {
+      if (
+        e.target.tagName?.toString().toLowerCase() == "a" ||
+        currentState.isLocked
+      )
+        return;
+      handleEvent(e);
       cardElement.classList.toggle("flipped");
-      window.setTimeout(resetBufferTimeout, timeoutDuration);
     },
     true
   );
